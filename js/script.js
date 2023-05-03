57e51bb294dca7f0f5cbbcb9be893022e2569a7b
alinlinca10 <71827000+alinlinca10@users.noreply.github.com>
2023-05-03 21:23:22 +0300

    Changed to DevTech
    Changed to DevTech

--- js/script.js
+++ js/script.js
@@ -36,37 +36,37 @@ new simpleParallax(image, {
 
 // ? Initialize Swiper
 
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 'auto',
-  spaceBetween: 30,
-  centeredSlides: 'auto',
-  loop: true,
-  // preloadImages: true,
-  slidesPerGroupSkip: 1,
-  grabCursor: true,
-  keyboard: {
-    enabled: true,
-  },
-  breakpoints: {
-    769: {
-      slidesPerView: 2,
-      slidesPerGroup: 1,
-    },
-  },
-  scrollbar: {
-    el: ".swiper-scrollbar",
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    // type: "progressbar",
-    type: "fraction",
-  },
-});
+// var swiper = new Swiper(".mySwiper", {
+//   slidesPerView: 'auto',
+//   spaceBetween: 30,
+//   centeredSlides: 'auto',
+//   loop: true,
+//   // preloadImages: true,
+//   slidesPerGroupSkip: 1,
+//   grabCursor: true,
+//   keyboard: {
+//     enabled: true,
+//   },
+//   breakpoints: {
+//     769: {
+//       slidesPerView: 2,
+//       slidesPerGroup: 1,
+//     },
+//   },
+//   scrollbar: {
+//     el: ".swiper-scrollbar",
+//   },
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//     // type: "progressbar",
+//     type: "fraction",
+//   },
+// });
 
 // ? Apple scroll video animation
 
